89d32aedfb27d722b796fdc14654bb8bda7f4a23
untung sukir <sukiruntung@gmail.com>
2026-01-28 20:29:09 +0700

    perbaikan

--- app/Filament/Resources/Penjualan/PenjualanBarangResource/Pages/CreatePenjualanBarang.php
+++ app/Filament/Resources/Penjualan/PenjualanBarangResource/Pages/CreatePenjualanBarang.php
@@ -213,11 +213,8 @@ class CreatePenjualanBarang extends CreateRecord
 
             $data['user_id'] = Auth::id();
             $data['penjualan_barang_no'] = NumberGenerator::generate($data['transaction_type'], $data['outlet_id']);
-            dd($data);
             unset($data['transaction_type']);
-            dd($data);
             $penerimaan = PenjualanPenjualanBarang::create($data);
-            dd($data['products']);
             foreach ($data['products'] as $detailproduct) {
 
                 if ($detailproduct['qty'] <= 0) {
@@ -244,7 +241,6 @@ class CreatePenjualanBarang extends CreateRecord
                     'user_id' =>  $data['user_id'],
                 ]);
             }
-            dd('kene');
             session()->forget('list_productsPenjualan');
             return $penerimaan;
         });
